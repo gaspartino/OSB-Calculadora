c665c5fa5acb17accf1df797bc9644dd6e698ec7
André Vinicius Santos de Lima <aviniciuslima@gmail.com>
2021-12-15 16:33:29 -0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Sum = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <button type= "submit">+</button>
+  )
 }
 
-export default App;
+const App1 = () => {
+  const [n1, setN1] = React.useState(0);
+  const [n2, setN2] = React.useState(0);
+  const [result, setResult] = React.useState('Resultado');
+
+  const handleNumberChange = (e : React.ChangeEvent<HTMLInputElement>) :void => {
+    if("n1" === e.target.id){
+      setN1(parseInt(e.target.value))  
+    } else {
+      setN2(parseInt(e.target.value))
+    }
+  }
+
+  const handleChangeResult = (e :React.MouseEvent<HTMLFormElement>) : void => {
+    setResult(String(n1 + n2))
+    e.preventDefault();
+  };
+
+  let title : string = process.env.REACT_APP_APP_TITTLE as string;
+
+  return (
+    <div className="App">
+      <h1>{title}</h1>
+      <div onChange= {handleNumberChange}>
+        <input type= "number" id= "n1"/>
+        <input type= "number" id= "n2"/>
+      </div>
+      
+      <form onClick={handleChangeResult}>
+        <Sum /><br/>
+      </form>
+      <div className= "result">
+        {result}
+      </div>
+    </div>
+  );
+} 
+
+export default App1;
